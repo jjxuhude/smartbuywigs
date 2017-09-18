50082eb194cc27d3f63e737d0133f7eccf39a0e9
jjxuhude <jjxuhuade@163.com>
2017-09-19 00:08:01 +0800

    customer

--- app/code/core/Mage/Customer/Block/Form/Login.php
+++ app/code/core/Mage/Customer/Block/Form/Login.php
@@ -87,4 +87,36 @@ class Mage_Customer_Block_Form_Login extends Mage_Core_Block_Template
         }
         return $this->_username;
     }
+
+    public function getFormData()
+    {
+        $data = $this->getData('form_data');
+        if (is_null($data)) {
+            $formData = Mage::getSingleton('customer/session')->getCustomerFormData(true);
+            $data = new Varien_Object();
+            if ($formData) {
+                $data->addData($formData);
+                $data->setCustomerData(1);
+            }
+            if (isset($data['region_id'])) {
+                $data['region_id'] = (int)$data['region_id'];
+            }
+            $this->setData('form_data', $data);
+        }
+        return $data;
+    }
+
+    public function isNewsletterEnabled()
+    {
+        return Mage::helper('core')->isModuleOutputEnabled('Mage_Newsletter');
+    }
+
+    public function getBackUrl()
+    {
+        $url = $this->getData('back_url');
+        if (is_null($url)) {
+            $url = $this->helper('customer')->getLoginUrl();
+        }
+        return $url;
+    }
 }
